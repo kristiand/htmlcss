4953aa20f58c27444d6485da9d14056728153c6a
Del Barrio Delgado <k-barrio@habe.eus>
2024-12-19 14:43:03 +0100

    Tailwin hasiera

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,15 +3,10 @@
  * see commented examples below
  */
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+//import * as bootstrap from 'bootstrap';
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
 /**
  * Write any other JavaScript below
  */
-
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
